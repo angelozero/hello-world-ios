c1de71a85e6bf6d2bf1d8aef9c2d72ce5092a830
Angelo Zero <angelo.akm@gmail.com>
2025-08-15 23:39:51 -0300

    first commit

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,11 @@
 import UIKit
 
 /**
- Iniciando com programação Swift para iOs
+ Criação de variáveis
  */
 
-var text = "Ola, Angelo"
-print(text)
-
+var name = "Angelo"
+print(name)
 
 //Declarando uma variável mutável ---> 'var'
 var message = "Isso é um texto"
@@ -16,8 +15,8 @@ message = "Agora um novo texto!"
 print(message)
 
 // Declarando uma variável constante ---> 'let'
-let name = "Angelo Zero"
-print(name)
+let fullName = "Angelo Zero"
+print(fullName)
 
 /*
  Isso não funciona, let é constante e não pode ser alterado após ser criado
@@ -28,3 +27,59 @@ print(name)
 
 let pi = 3.14
 print(pi)
+
+
+print()
+/**
+ Declarando variáveis com seu tipos de dado
+ */
+
+// Numeros inteiro
+let int: Int = 1
+print(int)
+
+// Numeros com casas decimais
+let double: Double = 3.14
+print(double)
+
+// Texto
+var text: String = "a, b, c, d, e, ... y, z"
+print(text)
+
+// Lógico
+var isHuman: Bool = true
+print(isHuman)
+
+
+print()
+
+/**
+ Convertendo tipos de dados
+ */
+
+let num: Int = 10
+let numName: String = String(num)
+print("O texto é " + numName)
+
+let textValue: String = "10"
+let value: Int = Int(textValue) ?? 0
+print("O valor é \(value)")
+
+print()
+
+/**
+ 1. Criar um programa para converter em Boolean uma String no formato "true". 
+ Ou seja, na saída precisamos ter um tipo Boolean a partir de uma String​.
+
+ 2. Criar um programa para converter um número decimal (20.1) em um número inteiro, isto é, número que não aceita casas decimais. 
+ Dica: lembrando que números inteiros são do tipo Int e números decimais são do tipo Double.
+ */
+
+// 1
+let booleanValue: Bool = Bool("true") ?? false
+print("Resposta exercicio 1 ---> \(booleanValue)")
+
+// 2
+let piValue: Double = 3.14
+let intNumber: Int = Int(piValue)
+print("Resposta exercicio 2 ---> \(intNumber)")
