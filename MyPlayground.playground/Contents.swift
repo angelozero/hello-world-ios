1787f038240dacdd3747bbe1a8bcb322f5772dc0
Angelo Zero <angelo.akm@gmail.com>
2025-08-15 22:45:37 -0300

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+/**
+ Iniciando com programação Swift para iOs
+ */
+
+var text = "Ola, Angelo"
+print(text)
+
+
+//Declarando uma variável mutável ---> 'var'
+var message = "Isso é um texto"
+print(message)
+
+message = "Agora um novo texto!"
+print(message)
+
+// Declarando uma variável constante ---> 'let'
+let name = "Angelo Zero"
+print(name)
+
+/*
+ Isso não funciona, let é constante e não pode ser alterado após ser criado
+ Cannot assign to value: 'name' is a 'let' constant
+ name = "Jake o Cao"
+ print(name)
+ */
+
+let pi = 3.14
+print(pi)
